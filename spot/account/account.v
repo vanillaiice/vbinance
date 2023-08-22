68b57e70390d7f229cd66beebc3eaa513ef7d581
vanilla <vanillaiice@tutanota.com>
2023-08-22 19:30:52 +0300

    feat: make fields in balances struct public

--- spot/account/account.v
+++ spot/account/account.v
@@ -8,6 +8,7 @@ import server_time as st
 struct Response {
 pub:
 	balances []struct {
+	pub:
 		asset string
 		free  string
 	}
@@ -15,7 +16,7 @@ pub:
 
 pub fn info(server_base_endpoint string, secret_key string, api_key string) (Response, string, int) {
 	timestamp := st.get(server_base_endpoint)
-	req := 'timestamp=${timestamp}' 
+	req := 'timestamp=${timestamp}'
 	sig := sign.sign(secret_key, req)
 	mut request := http.new_request(.get, 'https://${server_base_endpoint}/api/v3/account?${req}&signature=${sig}',
 		'')
@@ -30,11 +31,11 @@ pub fn info(server_base_endpoint string, secret_key string, api_key string) (Res
 
 pub fn info_pretty(server_url string, secret_key string, api_key string) string {
 	info_raw, err, _ := info(server_url, secret_key, api_key)
-	
+
 	if info_raw.balances.len == 0 {
 		return '${err}'
 	}
-	
+
 	mut info_pretty := []string{}
 
 	for i in info_raw.balances {

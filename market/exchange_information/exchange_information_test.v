ccd976e73ba318d5c92288ff4b871a5d2114c8ef
vanilla <vanillaiice@tutanota.com>
2023-08-25 07:11:45 +0300

    style: format with vfmt

--- market/exchange_information/exchange_information_test.v
+++ market/exchange_information/exchange_information_test.v
@@ -1,13 +1,13 @@
 module exchange_information
 
 fn test_get() {
-  url := 'testnet.binance.vision'
-  pairs := ['TRXUSDT', 'BTCUSDT']
-  
-  _, _, code := get(url, pairs)
-  assert code == 200
-  
-  s, _ := step_size(url, pairs)
-  assert s[pairs[1]] == '0.00000100'
-  assert s[pairs[0]] == '0.10000000'
+	url := 'testnet.binance.vision'
+	pairs := ['TRXUSDT', 'BTCUSDT']
+
+	_, _, code := get(url, pairs)
+	assert code == 200
+
+	s, _ := step_size(url, pairs)
+	assert s[pairs[1]] == '0.00000100'
+	assert s[pairs[0]] == '0.10000000'
 }
